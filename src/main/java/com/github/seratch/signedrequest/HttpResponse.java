6bb46e88dd598a2f963ec46e5e5022f6afb879d0
Kazuhiro SERA <seratch@gmail.com>
2011-03-04 00:23:21 +0900

    Added copyright

--- src/main/java/com/github/seratch/signedrequest/HttpResponse.java
+++ src/main/java/com/github/seratch/signedrequest/HttpResponse.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Kazuhiro Sera
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package com.github.seratch.signedrequest;
 
 import java.util.HashMap;
@@ -6,34 +21,34 @@ import java.util.Map;
 
 public class HttpResponse {
 
-	public Integer statusCode;
+    public Integer statusCode;
 
-	public Map<String, List<String>> headers = new HashMap<String, List<String>>();
+    public Map<String, List<String>> headers = new HashMap<String, List<String>>();
 
-	private String content;
+    private String content;
 
-	public Integer getStatusCode() {
-		return statusCode;
-	}
+    public Integer getStatusCode() {
+        return statusCode;
+    }
 
-	public void setStatusCode(Integer statusCode) {
-		this.statusCode = statusCode;
-	}
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
 
-	public Map<String, List<String>> getHeaders() {
-		return headers;
-	}
+    public Map<String, List<String>> getHeaders() {
+        return headers;
+    }
 
-	public void setHeaders(Map<String, List<String>> headers) {
-		this.headers = headers;
-	}
+    public void setHeaders(Map<String, List<String>> headers) {
+        this.headers = headers;
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public String getContent() {
+        return content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
 }
